549bf0b3233cb361813e0664f3d879cb41d720c3
woywro <woywro@gmail.com>
2022-02-07 17:13:08 +0100

    fix: generate search query fix

--- src/pages/search/[search].tsx
+++ src/pages/search/[search].tsx
@@ -15,59 +15,40 @@ export default SearchView;
 export async function getServerSideProps(context) {
   const data: ItemType[] = [];
   const tagArray = context.params.search.replace("-", " ").split(" ");
-  if (tagArray.length == 1) {
+
+  const createQuery = () => {
     const colRef = collection(db, "Courses");
-    const q = query(colRef, where(`tags1.${tagArray[0]}`, "==", true));
-    const docSnap = await getDocs(q);
-    docSnap.forEach((doc) => {
-      data.push(doc.data());
-    });
-    if (!data) {
-      return {
-        notFound: true,
-      };
+    if (tagArray.length == 1) {
+      return query(colRef, where(`tags1.${tagArray[0]}`, "==", true));
+    } else if (tagArray.length == 2) {
+      return query(
+        colRef,
+        where(`tags1.${tagArray[0]}`, "==", true),
+        where(`tags1.${tagArray[1]}`, "==", true)
+      );
+    } else if (tagArray.length == 3) {
+      return query(
+        colRef,
+        where(`tags1.${tagArray[0]}`, "==", true),
+        where(`tags1.${tagArray[1]}`, "==", true),
+        where(`tags1.${tagArray[2]}`, "==", true)
+      );
+    } else {
+      return;
     }
+  };
+
+  const q = createQuery();
+  const docSnap = await getDocs(q);
+  docSnap.forEach((doc) => {
+    data.push(doc.data());
+  });
+  if (!data) {
     return {
-      props: { data },
-    };
-  } else if (tagArray.length == 2) {
-    const colRef = collection(db, "Courses");
-    const q = query(
-      colRef,
-      where(`tags1.${tagArray[0]}`, "==", true),
-      where(`tags1.${tagArray[1]}`, "==", true)
-    );
-    const docSnap = await getDocs(q);
-    docSnap.forEach((doc) => {
-      data.push(doc.data());
-    });
-    if (!data) {
-      return {
-        notFound: true,
-      };
-    }
-    return {
-      props: { data },
-    };
-  } else if (tagArray.length == 3) {
-    const colRef = collection(db, "Courses");
-    const q = query(
-      colRef,
-      where(`tags1.${tagArray[0]}`, "==", true),
-      where(`tags1.${tagArray[1]}`, "==", true),
-      where(`tags1.${tagArray[2]}`, "==", true)
-    );
-    const docSnap = await getDocs(q);
-    docSnap.forEach((doc) => {
-      data.push(doc.data());
-    });
-    if (!data) {
-      return {
-        notFound: true,
-      };
-    }
-    return {
-      props: { data },
+      notFound: true,
     };
   }
+  return {
+    props: { data },
+  };
 }
